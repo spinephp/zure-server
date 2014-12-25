aed80f931153ec2eac4f077905a9a387ed4634a5
lxm_office <lxm.yrr@gmail.com>
2014-12-25 21:04:05 +0800

    add const.right.php

---
+++ base/const.right.php
@@ -0,0 +1,26 @@
+<?php
+namespace woo\base;
+
+		const RIGHT_WWW_SHOW = 0x80000000;		// 查看站点信息
+		const RIGHT_WWW_EDIT = 0x40000000;		// 编辑站点信息
+		const RIGHT_NEW_SHOW = 0x20000000;		// 查看新闻信息
+		const RIGHT_NEW_EDIT = 0x10000000;		// 编辑新闻信息
+		const RIGHT_WORD_SHOW = 0x08000000;		// 查看留言
+		const RIGHT_WORD_EDIT = 0x04000000;		// 编辑留言
+
+		const RIGHT_CUSTOM_SHOW = 0x02000000;		// 显示客户信息
+		const RIGHT_CUSTOM_EDIT = 0x01000000;		// 编辑客户信息
+		const RIGHT_PRODUCTCLASS_SHOW = 0x00800000;		// 查看产品类
+		const RIGHT_PRODUCTCLASS_EDIT = 0x00400000;		// 编辑产品类
+		const RIGHT_PRODUCT_SHOW = 0x00200000;		// 查看产品
+		const RIGHT_PRODUCT_EDIT = 0x00100000;		// 编辑产品
+		const RIGHT_ORDER_SHOW = 0x00080000;		//  查看订单
+		const RIGHT_ORDER_EDIT = 0x00040000;		// 编辑订单
+
+		const RIGHT_PRODUCE_SHOW = 0x00020000;		// 查看生产进度
+		const RIGHT_PRODUCE_EDIT = 0x00010000;		// 编辑生产进度
+
+		const RIGHT_EMPLOYEE_SHOW = 0x00008000;		// 查看雇员信息
+		const RIGHT_EMPLOYEE_EDIT = 0x00004000;		// 编辑雇员信息
+		const RIGHT_EMPLOYEERIGHT_EDIT = 0x00002000;		// 设置管理者权限
+?>
