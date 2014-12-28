61ddbd6684a2161646607aef3f1058c9822e409f
spinephp <yrrlyg@gmail.com>
2014-12-28 22:28:20 +0800

    add p_order

---
+++ view/hjOrder.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset=utf-8>
+<meta name="robots" content="all" />
+<Meta name="Description" Content="连云港云瑞耐火材料有限公司,云瑞,耐火材料,耐火,窑具,棚板,推板,电解槽内衬砖,氮化硅结合碳化硅,氮化硅,碳化硅制品">
+<Meta name="Keywords" Lang="EN" Content="yrr,SiC,Si3N4">
+<Meta name="Keywords" Lang="zh-CN" Content="连云港云瑞耐火材料有限公司,云瑞,耐火材料,耐火,窑具,棚板,推板,电解槽内衬砖,氮化硅结合碳化硅,氮化硅,碳化硅制品">
+  <title>YRR App</title>
+  <link rel="stylesheet" type="text/css" href="themes/redmond/jquery-ui.min.css" />
+  <link rel="stylesheet" href="css/order.css" type="text/css" charset="utf-8">
+  <script src="scripts/order.js" charset="utf-8"></script>
+  <script charset="utf-8">
+    var exports = this;
+    require("lib/jquery-ui.min");
+    jQuery(function(){
+      var App = require("index");
+      exports.app = new App({el: $("body")});
+    });
+  </script>
+</head>
+<body>
+</body>
+</html>
